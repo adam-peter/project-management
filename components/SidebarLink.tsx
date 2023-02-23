e7429bfae6d0f5063829412af94131bd72c82366
adam.petrzela <adam.petrzela@ais-servis.cz>
2023-02-23 18:46:22 +0100

    SidebarLink component

---
+++ components/SidebarLink.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React from "react";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { Settings, User, Grid, Calendar } from "react-feather";
+
+const icons = {
+  Settings,
+  User,
+  Grid,
+  Calendar,
+};
+
+interface SidebarLinkProps {
+  link: {
+    link: string;
+    icon: "Settings" | "User" | "Grid" | "Calendar";
+  };
+}
+
+const SidebarLink: React.FC<SidebarLinkProps> = ({ link }) => {
+  const pathname = usePathname(); //doesn't have to be a state - it reloads every time the path changes anyways
+  let isActive = false;
+
+  if (pathname === link.link) {
+    isActive = true;
+  }
+
+  const Icon = icons[link.icon];
+
+  return (
+    <Link href={link.link}>
+      <Icon
+        size={40}
+        className={`stroke-gray-400 transition duration-200 ease-in-out hover:stroke-violet-600 ${
+          isActive && "stroke-violet-600"
+        }`}
+      />
+    </Link>
+  );
+};
+
+export default SidebarLink;
